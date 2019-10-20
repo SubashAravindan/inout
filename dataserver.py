c9c6aea9389212b2ef5b13ef674006bece97c0b1
Subash Aravindan <p.subasharavindan@gmail.com>
2019-10-20 14:30:51 +0530

    Add new objects

--- dataserver.py
+++ dataserver.py
@@ -3,8 +3,12 @@ from flask import request, make_response, jsonify
 import base64
 import csv
 import json
+from flask_cors import CORS
+import os
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.getcwd()+'/Training_Data/'
 
+CORS(app)
 
 @app.route('/')
 def hello_world():
@@ -16,5 +20,25 @@ def hello_world():
                 file1 = open(json.loads(row[2])[0].encode('utf-8').strip(), 'rb').read()
                 image = base64.b64encode(file1).decode('utf')
                 response = jsonify([row[0],row[1],image])
-                response.headers.add('Access-Control-Allow-Origin', 'null')
+                # response.headers.add('Access-Control-Allow-Origin', 'null')
                 return response
+
+@app.route('/new',methods=["POST"])
+def new_image():
+    # print(request.files.getlist("file"))
+    name = request.form["name"]
+    description = request.form["description"]
+    file_paths = []
+    for file in request.files.getlist("file"):
+        if not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], name)):
+            os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'], name))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], name + '/' +file.filename))
+        file_paths.append(os.path.join(app.config['UPLOAD_FOLDER'], name + '/' +file.filename))
+    
+    fields=[name, description,json.dumps(file_paths)]
+    print(fields)
+    # print(json.loads(request.data.decode('utf')))
+    with open('data.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+    return "request.data"
