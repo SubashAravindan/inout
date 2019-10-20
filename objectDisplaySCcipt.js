ddeadf8ea6fd747d2b2b5a79cd9a5aa9b4a432b4
Subash Aravindan <p.subasharavindan@gmail.com>
2019-10-20 09:31:21 +0530

    Finish objects page

--- objectDisplaySCcipt.js
+++ objectDisplaySCcipt.js
@@ -9,6 +9,8 @@ fetch('http:localhost:8897?name=' + objectName, {
 .then(data => {
     let img = new Image()
     img.src = 'data:image/jpeg;base64,' + data[2]
-    document.querySelector('body').append(img)
+    document.querySelector('.img-display').append(img)
+    document.querySelector('.name-display').innerHTML=data[0]
+    document.querySelector('.description-display').innerHTML=data[1]
 })
 
